24fdf7bc274197515a221b48a7e494252b22c21d
Zachary Cox <zachary908@gmail.com>
2013-08-26 10:47:38 -0400

    f

--- pokerMethods.php
+++ pokerMethods.php
@@ -382,10 +382,11 @@
                 $SqlNotes = $_REQUEST['phpNotes'];
                 $AddSessionMsg = "";
 
-				$strQuery = "CALL AddSession('".$MemberId."', '".$SqlStartDate."', '".
+				$strQuery = "CALL AddSession('".$MemberId."', '".$SqlStartDate."', '".$SqlEndDate."', '".$SqlLocation."', '".$SqlGameType."', '".$SqlRingTour.
+					"', '".$SqlLimits."', '".$SqlBuyin."', '".$SqlCashout."', '".$SqlPlace."', '".$SqlNotes."', @AddSessionMsg);";
 
-                if($stmt == false){
-                    die(print_r(sqlsrv_errors(), true));
+                if (!$mysqli->multi_query($strQuery)) {
+                    echo "CALL failed: (".$mysqli->errno.") ".$mysqli->error;
                 }
 
                 if($AddSessionMsg == 1){
@@ -454,41 +455,50 @@
                 $MemberId = $_SESSION['user']['id'];
                 $GetSessionsMsg = 0;
 
-                $params = array(
-                    array($MemberId, SQLSRV_PARAM_IN, SQLSRV_PHPTYPE_STRING(SQLSRV_ENC_CHAR), SQLSRV_SQLTYPE_UNIQUEIDENTIFIER),
-                    array($GetSessionsMsg, SQLSRV_PARAM_OUT, SQLSRV_PHPTYPE_INT, SQLSRV_SQLTYPE_BIT)
-                );
-
-                $stmt = mysqli_query($mysqli, '{CALL GetSessions(?,?)}', $params);
-
-                if($stmt === false){
-                    echo 'Data could not be retrieved from database.';
-                    die(print_r(sqlsrv_errors(), true));
+				$strQuery = "CALL GetSessions('".$MemberId."', @GetSessionsMsg);";
+				
+                if (!$mysqli->multi_query($strQuery)) {
+                    echo "CALL failed: (".$mysqli->errno.") ".$mysqli->error;
                 }
+				
+				do {
+					/* store first result set */
+					if ($result = $mysqli->store_result()) {
+						while ($row = $result->fetch_row()) {
+							printf($row[0]."#".$row[1]."#".$row[2]."#".$row[3]."#".$row[4]."#".$row[5]."#".$row[6]."#".$row[7]."#".$row[8]."#".$row[9]."#"
+							.$row[10]."#".$row[11]."#".$row[12]."#".$row[13]."#");
+						}
+						$result->free();
+					}
+					/* print divider */
+					if ($mysqli->more_results()) {
+						printf("-----------------\n");
+					}
+				} while ($mysqli->next_result());
 
                 // ADD AN OUTPUT PARAM- IF OUTPUT PARAM = 1, STATUS MSG READS: "NO SESSIONS FOUND, YOU SHOULD ADD A SESSION",
                 // OTHERWISE, ENTER THIS WHILE LOOP
-                if($GetSessionsMsg == 0){
-                    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
-                        echo $row['Id']."#"
-                            .$row['StartTime']."#"
-                            .$row['Location']."#"
-                            .$row['GameType']."#"
-                            .$row['Limits']."#"
-                            .$row['Duration']."#"
-                            .$row['BuyIn']."#"
-                            .$row['CashOut']."#"
-                            .$row['RingTour']."#"
-                            .$row['Place']."#"
-                            .$row['Rate']."#"
-                            .$row['Return']."#"
-                            .$row['LocType']."#"
-                            .$row['Notes']."%";
-                    }
-                }
-                else{
-                    echo $GetSessionsMsg;
-                }
+                // if($GetSessionsMsg == 0){
+                    // while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
+                        // echo $row['Id']."#"
+                            // .$row['StartTime']."#"
+                            // .$row['Location']."#"
+                            // .$row['GameType']."#"
+                            // .$row['Limits']."#"
+                            // .$row['Duration']."#"
+                            // .$row['BuyIn']."#"
+                            // .$row['CashOut']."#"
+                            // .$row['RingTour']."#"
+                            // .$row['Place']."#"
+                            // .$row['Rate']."#"
+                            // .$row['Return']."#"
+                            // .$row['LocType']."#"
+                            // .$row['Notes']."%";
+                    // }
+                // }
+                // else{
+                    // echo $GetSessionsMsg;
+                // }
 
                 $mysqli->close();
                 break;
