bcaf6ab8dca8452bbbb9d4cad2bbc6180bb31a4c
Zachary Cox <zachary908@gmail.com>
2013-08-31 13:43:35 -0400

    added ga enhanced link attr code

--- controls/header.php
+++ controls/header.php
@@ -6,6 +6,16 @@
 		<script type="text/javascript" src="scripts/javascript.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 		<script src="scripts/jquery-1.8.3.js"></script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+			_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+			ga('create', 'UA-43676050-1', 'zacharybriancox.com');
+			ga('send', 'pageview');
+		</script>
 	</head>
 
 	<body>
